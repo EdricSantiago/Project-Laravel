fe6af3c2fd9965fd8a86e0b6b16ac4ad6b3ed58f
Lyinceer <theyxeldev113@gmail.com>
2026-05-28 02:12:08 +0700

    feat: add TransactionController
    add TransactionController with deposit, withdraw, transfer, history

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class TransactionController extends Controller
 {
@@ -17,7 +21,7 @@ class TransactionController extends Controller
             ->latest()
             ->get();
 
-        return view('transaction.index', compact('user', 'account', 'transactions'));
+        return view('transaction', compact('user', 'account', 'transactions'));
     }
 
     public function deposit(Request $request): RedirectResponse
