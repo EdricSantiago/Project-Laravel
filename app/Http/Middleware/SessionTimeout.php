f67c6ec242558587c69e3818d409da87e8e8dedc
Kazuhash <Jerichoangdev@gmail.com>
2026-06-20 23:44:28 +0700

    add stting controller,pincooldown,sessiontimeout

---
+++ app/Http/Middleware/SessionTimeout.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionTimeout
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            $timeout = 5 * 60; 
+
+            if ($request->session()->has('last_activity')) {
+                $lastActivity = $request->session()->get('last_activity');
+
+                if (time() - $lastActivity > $timeout) {
+                    Auth::logout();
+                    $request->session()->invalidate();
+                    $request->session()->regenerateToken();
+                    return redirect()->route('login')->withErrors(['email' => 'Sesi Anda telah berakhir karena tidak aktif.']);
+                }
+            }
+
+            $request->session()->put('last_activity', time());
+        }
+
+        return $next($request);
+    }
+}
