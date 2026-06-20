05b8606509c0c0e671019779c5613c15c4e673f7
Kazuhash <Jerichoangdev@gmail.com>
2026-06-20 23:50:08 +0700

    add change password and security log

--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,23 @@ Route::middleware('auth')->group(function () {
     Route::post('/account/topup',   [AccountsController::class, 'topup'])->name('account.topup');
     Route::post('/account/withdraw',[AccountsController::class, 'withdraw'])->name('account.withdraw');
 
-    Route::get('/transaction', [TransactionController::class, 'index'])->name('transaction.index');
-    Route::post('/transaction/deposit', [TransactionController::class, 'deposit'])->name('transaction.deposit');
-    Route::post('/transaction/withdraw', [TransactionController::class, 'withdraw'])->name('transaction.withdraw');
-    Route::post('/transaction/transfer', [TransactionController::class, 'transfer'])->name('transaction.transfer');
-    Route::get('/transaction/history', [TransactionController::class, 'history'])->name('transaction.history');
+    Route::get('/transaction',              [TransactionController::class, 'index'])->name('transaction.index');
+    Route::post('/transaction/deposit',     [TransactionController::class, 'deposit'])->name('transaction.deposit');
+    Route::post('/transaction/withdraw',    [TransactionController::class, 'withdraw'])->name('transaction.withdraw')
+        ->middleware('pin.cooldown');
+    Route::post('/transaction/transfer',    [TransactionController::class, 'transfer'])->name('transaction.transfer')
+        ->middleware('pin.cooldown');
+    Route::get('/transaction/history',      [TransactionController::class, 'history'])->name('transaction.history');
+    Route::post('/transaction/pay-insurance',[TransactionController::class, 'payInsurance'])->name('transaction.payInsurance');
+    Route::get('/transaction/export-pdf',   [TransactionController::class, 'exportPdf'])->name('transaction.exportPdf');
 
-    Route::post('/transaction/pay-insurance', [TransactionController::class, 'payInsurance'])->name('transaction.payInsurance');
-});
-
-Route::middleware('auth')->group(function () {
-        Route::prefix('security')->group(function () {
-        Route::post('/setup-pin', [SecurityController::class, 'setupPin'])->name('security.setup');
-        Route::patch('/panic', [SecurityController::class, 'freezeAccount'])->name('security.panic');
-        Route::put('/change-pin', [SecurityController::class, 'changePin'])->name('security.change');
-        Route::post('/verify-pin', [SecurityController::class, 'verifyPin'])->name('security.verify');
-        Route::get('/status', [SecurityController::class, 'getSecurityStatus'])->name('security.status');
+    Route::prefix('security')->group(function () {
+        Route::post('/setup-pin',   [SecurityController::class, 'setupPin'])->name('security.setup');
+        Route::post('/panic',       [SecurityController::class, 'freezeAccount'])->name('security.panic');
+        Route::post('/change-pin',  [SecurityController::class, 'changePin'])->name('security.change');
+        Route::post('/verify-pin',  [SecurityController::class, 'verifyPin'])->name('security.verify');
+        Route::get('/status',       [SecurityController::class, 'getSecurityStatus'])->name('security.status');
+        Route::post('/security/change-password', [SecurityController::class, 'changePassword'])->name('security.password');
+        Route::get('/settings/security-log', [SettingsController::class, 'securityLog'])->name('settings.security-log');
     });
-
 });
